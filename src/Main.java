d29e9686298984aa464944dcfaf260c0bb4afe7e
Callum McGregor <callummcgregor50@gmail.com>
2018-03-14 10:56:12 +0000

    reverse factorial

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,33 @@
 
 public class Main {
 
+	private static int loops;
+
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		int number = 24;
+
+		if (reverseFac(number)) {
+			System.out.println(number + " = " + loops + "!");
+		} else {
+			System.out.println(number + " NONE");
+		}
 
 	}
 
+	private static boolean reverseFac(int number) {
+
+		int newNum = number;
+		for(loops = 1; loops <= newNum; loops++) {
+			if (newNum % loops != 0) {
+				return false;
+			} else {
+				if (newNum == loops) {
+					return true;
+				}
+			}
+			newNum = newNum / loops;
+		}
+		return false;
+	}
 }
