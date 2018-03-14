7dea6d94488c8c0abf1314ca287c5b06506a6839
Callum McGregor <callummcgregor50@gmail.com>
2018-03-14 13:46:00 +0000

    Added endingCheck

--- src/DogShow.java
+++ src/DogShow.java
@@ -5,26 +5,17 @@ public class DogShow {
 	static String[] numbers;
 	
 	public static void main(String[] args) {
-		numbers = new String[1000];
+		numbers = new String[10000];
 		
 		Random rand = new Random();
 		int dog = rand.nextInt(numbers.length-1);
 		
-		
 		for(int i = 0; i < numbers.length; i++) {
-			String current;
 			if(i == dog) {
 				numbers[i] = "Doggo";
 				continue;
 			}
-			
-			String number = Integer.toString(i + 1);
-			if( number.length() > 1 && number.charAt(number.length()-2) == '1') {
-				current = number + "th";
-			} else {
-				current = number + findEnding(number.charAt(number.length()-1));
-			}
-			numbers[i] = current;
+			numbers[i] = endingCheck(i);
 		}	
 		printDogs();
 	}
@@ -36,12 +27,7 @@ public class DogShow {
 				System.out.println();
 			}
 			if(numbers[i] == "Doggo") {
-				String number = Integer.toString(i + 1);
-				if(number.charAt(number.length()-2) == '1' && number.length() > 1) {
-					doggoLocation = number + "th";
-				} else {
-					doggoLocation = number + findEnding(number.charAt(number.length()-1));
-				}
+				doggoLocation = endingCheck(i);
 				continue;
 			}
 			System.out.print(numbers[i] + " ");
@@ -49,6 +35,15 @@ public class DogShow {
 		}
 		System.out.println();
 		System.out.println("Your dog finished: " + doggoLocation);
+	}
+	
+	private static String endingCheck(int i) {
+		String number = Integer.toString(i + 1);
+		if(number.length() > 1 && number.charAt(number.length()-2) == '1') {
+			return number + "th";
+		} else {
+			return number + findEnding(number.charAt(number.length()-1));
+		}
 	}
 	
 	private static String findEnding(char number) {
